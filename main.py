bf91c9337505443d2b647fe8abfa52e5b1e01ef2
Raed2001 <raed00635@gmail.com>
2022-04-14 16:56:44 +0200

    s

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 import os
 import subprocess
 
-os.system("mkdir verzeichnis")
-os.mkdir("verzeichnis_name")
+# os.system("mkdir new_directory")
 os.getpid()
-os.chmod("", "")
+##os.chmod("u", "+rwx")
 
 p = subprocess.Popen(
-['ls', '-la'],
-stdout=subprocess.PIPE)
+    ['ls', '-la'],
+    stdout=subprocess.PIPE, shell=True)
 output = p.communicate()[0]
 print(output)
