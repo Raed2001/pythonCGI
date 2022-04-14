e008b490e178f6bc97661a7852cf7335aebf88e7
Raed2001 <raed00635@gmail.com>
2022-04-14 17:16:19 +0200

    s

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import subprocess
 # os.system("mkdir new_directory")
 os.getpid()
 ##os.chmod("u", "+rwx")
+os.chdir(r"C:\Users")
 
 p = subprocess.Popen(
-    ['ls', '-la'],
+    [r'cd C:\Users','dir'],
     stdout=subprocess.PIPE, shell=True)
 output = p.communicate()[0]
 print(output)
